f2c9635c48b7424d6eb16550adacb57b29d55da2
Pursue1221 <49906332+Pursue1221@users.noreply.github.com>
2024-12-12 14:54:29 +0800

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python -u train.py --dataset NewYork 
-python evaluation.py --datasets NewYork --task SemLoc &
-python evaluation.py --datasets NewYork --task EpiSim &
-python evaluation.py --datasets NewYork --task Stat &
-python evaluation.py --datasets NewYork --task LocRec &
-python evaluation.py --datasets NewYork --task NexLoc &
+python evaluation.py --datasets NewYork --task SemLoc 
+python evaluation.py --datasets NewYork --task EpiSim 
+python evaluation.py --datasets NewYork --task Stat 
+python evaluation.py --datasets NewYork --task LocRec 
+python evaluation.py --datasets NewYork --task NexLoc 
