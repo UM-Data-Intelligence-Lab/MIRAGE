9623d662667e11844a072385f19eb240f9bad3d8
Pursue1221 <49906332+Pursue1221@users.noreply.github.com>
2024-12-12 14:54:03 +0800

    Delete code/run_ist.sh

--- code/run_ist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python -u train.py --dataset Istanbul 
-python evaluation.py --datasets Istanbul --task SemLoc &
-python evaluation.py --datasets Istanbul --task EpiSim &
-python evaluation.py --datasets Istanbul --task Stat &
-python evaluation.py --datasets Istanbul --task LocRec --cuda 1 &
-python evaluation.py --datasets Istanbul --task NexLoc --cuda 1 &
